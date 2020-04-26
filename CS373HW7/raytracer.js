02573956973ea3d1abd8a3e6d32afa013a3bc56e
Kunj Dedhia <kdedhia@umass.edu>
2020-04-26 17:16:33 -0400

    ambient occlusion

---
+++ CS373HW7/raytracer.js
@@ -0,0 +1,213 @@
+/* Ray class:
+ * o: origin (THREE.Vector3)
+ * d: normalized direction (THREE.Vector3)
+ */
+class Ray {
+	constructor(origin, direction) {
+		this.o = origin.clone();
+		this.d = direction.clone();
+		this.d.normalize();
+	}
+	pointAt(t) {
+		// P(t) = o + t*d
+		let point = this.o.clone();
+		point.addScaledVector(this.d, t);
+		return point;
+	}
+	direction() { return this.d; }
+	origin() { return this.o; }
+}
+
+function render() {
+	// create canvas of size imageWidth x imageHeight and add to DOM
+	let canvas = document.createElement('canvas');
+	canvas.width = imageWidth;
+	canvas.height = imageHeight;
+	canvas.style = 'background-color:red';
+	document.body.appendChild(canvas);
+	let ctx2d = canvas.getContext('2d'); // get 2d context
+	let image = ctx2d.getImageData(0, 0, imageWidth, imageHeight); // get image data
+	let pixels = image.data; // get pixel array
+
+	let row=0;
+	let idx=0;
+	let chunksize=10; // render 10 rows at a time
+	console.log('Raytracing started...');
+	(function chunk() {
+		// render a chunk of rows
+		for(let j=row;j<row+chunksize && j<imageHeight;j++) {
+			for(let i=0;i<imageWidth;i++,idx+=4) { // i loop
+				// compute normalized pixel coordinate (x,y)
+				let x = i/imageWidth;
+				let y = (imageHeight-1-j)/imageHeight;
+				let ray = camera.getCameraRay(x,y);
+				let color = raytracing(ray, 0);
+				setPixelColor(pixels, idx, color);
+			}
+		}
+		row+=chunksize;  // non-blocking j loop
+		if(row<imageHeight) {
+			setTimeout(chunk, 0);
+			ctx2d.putImageData(image, 0, 0); // display intermediate image
+		} else {
+			ctx2d.putImageData(image, 0, 0); // display final image
+			console.log('Done.')
+		}
+	})();
+}
+
+/* Trace ray in the scene and return color of ray. 'depth' is the current recursion depth.
+ * If intersection material has non-null kr or kt, perform recursive ray tracing. */
+function raytracing(ray, depth) {
+	let color = new THREE.Color(0,0,0);
+// ===YOUR CODE STARTS HERE===
+	let isect = rayIntersectScene(ray);
+	if (isect != null) {
+		if ((isect.material.kr != null || isect.material.kt != null) && (depth < maxDepth)) {
+			if (isect.material.kr) {
+				let negDir = ray.d.clone();
+				negDir.negate();
+				let reflectCol = isect.material.kr.clone();
+				let reflect_ray = new Ray(isect.position, reflect(negDir, isect.normal));
+				reflectCol = reflectCol.multiply(raytracing(reflect_ray, depth+1));
+				color = color.add(reflectCol);
+			} 
+			if (isect.material.kt) {
+				let refractCol = isect.material.kt.clone();
+				let refract_ray = new Ray(isect.position, refract(ray.d, isect.normal, isect.material.ior));
+				refractCol = refractCol.multiply(raytracing(refract_ray, depth+1));
+				color = color.add(refractCol);
+			} 
+		} else {
+			let ambColor = ambientLight.clone();
+			let ambRef = isect.material.ka ? isect.material.ka : 0;
+			if (isect.material.ka) ambColor = ambColor.multiply(ambRef);
+			color = color.add(ambColor);
+
+			color = color.add(shading(ray, isect));
+		}
+		return color;
+	}
+	return backgroundColor;
+// ---YOUR CODE ENDS HERE---
+}
+
+/* Compute and return shading color given a ray and the intersection point structure. */
+function shading(ray, isect) {
+	let color = new THREE.Color(0,0,0);
+// ===YOUR CODE STARTS HERE===
+	for (let i = 0; i < lights.length; i++) {
+		let ls = lights[i].getLight(isect.position);
+		let shadowRay = new Ray(isect.position, ls.direction);
+		let distToLightVector = ls.position.clone();
+		distToLightVector.sub(isect.position);
+		let distToLight = distToLightVector.length();
+		let shadow_isect = rayIntersectScene(shadowRay);
+
+		if (shadow_isect && shadow_isect.t < distToLight) 
+			continue;
+
+		if (isect.material.kd) {
+			let diffColor = ls.intensity.clone();
+			diffColor = diffColor.multiply(isect.material.kd);
+			diffColor = diffColor.multiplyScalar(Math.max(isect.normal.dot(ls.direction), 0));
+			color = color.add(diffColor);
+		}
+
+		if (isect.material.ks) {
+			let specColor = ls.intensity.clone();
+			specColor = specColor.multiply(isect.material.ks);
+			let negDir = ray.d.clone();
+			negDir.negate();
+			specColor = specColor.multiplyScalar(Math.pow(Math.max(reflect(ls.direction, isect.normal).dot(negDir), 0), isect.material.p));
+			color = color.add(specColor);
+		}
+	}
+	// ambient occlusion
+	let sucSample = 0
+	let ambOcc = 0;
+	while (sucSample < 300) {
+		let theta = Math.acos(1 - (2*Math.random()));
+		let phi = 2*Math.PI*Math.random();
+
+		let x = Math.sin(theta)*Math.cos(phi);
+		let y = Math.cos(theta);
+		let z = Math.sin(theta)*Math.sin(phi);
+
+		let dirVec = new THREE.Vector3(x, y, z);
+		dirVec.normalize();
+		let angDirNorm = dirVec.dot(isect.normal);
+		if (angDirNorm > 0) {
+			let ambIsect = rayIntersectScene(new Ray(isect.position, dirVec));
+			if (ambIsect == null) {
+				ambOcc += angDirNorm;
+			}
+			sucSample += 1;
+		}
+	}
+	ambOcc = ambOcc/sucSample;
+	color = color.multiplyScalar(ambOcc*2);
+// ---YOUR CODE ENDS HERE---
+	return color;
+}
+
+/* Compute intersection of ray with scene shapes.
+ * Return intersection structure (null if no intersection). */
+function rayIntersectScene(ray) {
+	let tmax = Number.MAX_VALUE;
+	let isect = null;
+	for(let i=0;i<shapes.length;i++) {
+		let hit = shapes[i].intersect(ray, 0.0001, tmax);
+		if(hit != null) {
+			tmax = hit.t;
+			if(isect == null) isect = hit; // if this is the first time intersection is found
+			else isect.set(hit); // update intersection point
+		}
+	}
+	return isect;
+}
+
+/* Compute reflected vector, by mirroring l around n. */
+function reflect(l, n) {
+	// r = 2(n.l)*n-l
+	let r = n.clone();
+	r.multiplyScalar(2*n.dot(l));
+	r.sub(l);
+	return r;
+}
+
+/* Compute refracted vector, given l, n and index_of_refraction. */
+function refract(l, n, ior) {
+	let mu = (n.dot(l) < 0) ? 1/ior:ior;
+	let cosI = l.dot(n);
+	let sinI2 = 1 - cosI*cosI;
+	if(mu*mu*sinI2>1) return null;
+	let sinR = mu*Math.sqrt(sinI2);
+	let cosR = Math.sqrt(1-sinR*sinR);
+	let r = n.clone();
+	if(cosI > 0) {
+		r.multiplyScalar(-mu*cosI+cosR);
+		r.addScaledVector(l, mu);
+	} else {
+		r.multiplyScalar(-mu*cosI-cosR);
+		r.addScaledVector(l, mu);
+	}
+	r.normalize();
+	return r;
+}
+
+/* Convert floating-point color to integer color and assign it to the pixel array. */
+function setPixelColor(pixels, index, color) {
+	pixels[index+0]=pixelProcess(color.r);
+	pixels[index+1]=pixelProcess(color.g);
+	pixels[index+2]=pixelProcess(color.b);
+	pixels[index+3]=255; // alpha channel is always 255*/
+}
+
+/* Multiply exposure, clamp pixel value, then apply gamma correction. */
+function pixelProcess(value) {
+	value*=exposure; // apply exposure
+	value=(value>1)?1:value;
+	value = Math.pow(value, 1/2.2);	// 2.2 gamma correction
+	return value*255;
+}
---
+++ CS373HW7/shape.js
@@ -0,0 +1,224 @@
+/* Intersection structure:
+ * t:        ray parameter (float), i.e. distance of intersection point to ray's origin
+ * position: position (THREE.Vector3) of intersection point
+ * normal:   normal (THREE.Vector3) of intersection point
+ * material: material of the intersection object
+ */
+class Intersection {
+	constructor() {
+		this.t = 0;
+		this.position = new THREE.Vector3();
+		this.normal = new THREE.Vector3();
+		this.material = null;
+	}
+	set(isect) {
+		this.t = isect.t;
+		this.position = isect.position;
+		this.normal = isect.normal;
+		this.material = isect.material;
+	}
+}
+
+/* Plane shape
+ * P0: a point (THREE.Vector3) that the plane passes through
+ * n:  plane's normal (THREE.Vector3)
+ */
+class Plane {
+	constructor(P0, n, material) {
+		this.P0 = P0.clone();
+		this.n = n.clone();
+		this.n.normalize();
+		this.material = material;
+	}
+	// Given ray and range [tmin,tmax], return intersection point.
+	// Return null if no intersection.
+	intersect(ray, tmin, tmax) {
+		let temp = this.P0.clone();
+		temp.sub(ray.o); // (P0-O)
+		let denom = ray.d.dot(this.n); // d.n
+		if(denom==0) { return null;	}
+		let t = temp.dot(this.n)/denom; // (P0-O).n / d.n
+		if(t<tmin || t>tmax) return null; // check range
+		let isect = new Intersection();   // create intersection structure
+		isect.t = t;
+		isect.position = ray.pointAt(t);
+		isect.normal = this.n;
+		isect.material = this.material;
+		return isect;
+	}
+}
+
+/* Sphere shape
+ * C: center of sphere (type THREE.Vector3)
+ * r: radius
+ */
+class Sphere {
+	constructor(C, r, material) {
+		this.C = C.clone();
+		this.r = r;
+		this.r2 = r*r;
+		this.material = material;
+	}
+	intersect(ray, tmin, tmax) {
+// ===YOUR CODE STARTS HERE===
+		let temp = this.C.clone();
+		let a = ray.d.lengthSq();
+		let b = ray.o.clone();
+		b.sub(temp);
+		b = 2 * (b.dot(ray.d));
+
+		let c = ray.o.distanceToSquared(temp) - this.r2;
+
+		let delta = (b*b) - 4*a*c;
+		if (delta < 0) return null;
+
+		let t = null;
+		let t1 = (- b - Math.sqrt(delta))/2*a;
+		let t2 = (- b + Math.sqrt(delta))/2*a;
+
+		if (delta >= 0) {
+			if ((t1 > 0) && (t1 >= tmin && t1 <= tmax)) t = t1;
+			else if ((t2 > 0) && (t2 >= tmin && t2 <= tmax)) t = t2;
+			else return null;
+		}
+
+		let isect = new Intersection(); 
+		isect.t = t;
+		isect.position = ray.pointAt(t);
+		let normal = ray.pointAt(t);
+		normal.sub(temp);
+		normal.normalize();
+		isect.normal = normal;
+		isect.material = this.material;
+		return isect;
+
+// ---YOUR CODE ENDS HERE---
+	}
+}
+
+class Triangle {
+	/* P0, P1, P2: three vertices (type THREE.Vector3) that define the triangle
+	 * n0, n1, n2: normal (type THREE.Vector3) of each vertex */
+	constructor(P0, P1, P2, material, n0, n1, n2) {
+		this.P0 = P0.clone();
+		this.P1 = P1.clone();
+		this.P2 = P2.clone();
+		this.material = material;
+		if(n0) this.n0 = n0.clone();
+		if(n1) this.n1 = n1.clone();
+		if(n2) this.n2 = n2.clone();
+
+		// below you may pre-compute any variables that are needed for intersect function
+		// such as the triangle normal etc.
+// ===YOUR CODE STARTS HERE===
+		this.P2P0 = this.P2.clone();
+		this.P2P0.sub(this.P0);
+
+		this.P2P1 = this.P2.clone();
+		this.P2P1.sub(this.P1);
+		
+		this.normSmooth = false;
+		if (this.n0 && this.n1 && this.n0) {
+			this.normSmooth = true;
+		} else {
+			this.normDir = this.P2P0.clone();
+			this.normDir.cross(this.P2P1);
+			this.normDir.normalize();
+		}
+// ---YOUR CODE ENDS HERE---
+	}
+
+	intersect(ray, tmin, tmax) {
+// ===YOUR CODE STARTS HERE===
+		function det(arr) {
+			let det = arr[0][0] * (arr[1][1]*arr[2][2] - arr[1][2]*arr[2][1]) -
+			arr[0][1] * (arr[1][0]*arr[2][2] - arr[1][2]*arr[2][0]) +
+			arr[0][2] * (arr[1][0]*arr[2][1] - arr[1][1]*arr[2][0]);
+
+			return det;
+		}
+
+		let matrixEq = [
+			[ray.d.x, this.P2P0.x, this.P2P1.x],
+			[ray.d.y, this.P2P0.y, this.P2P1.y],
+			[ray.d.z, this.P2P0.z, this.P2P1.z]
+		];
+
+		let detEq = det(matrixEq);
+		if (detEq == 0) return null;
+
+		let P2O = this.P2.clone();
+		P2O.sub(ray.o);
+
+		let matT = [
+			[P2O.x, this.P2P0.x, this.P2P1.x],
+			[P2O.y, this.P2P0.y, this.P2P1.y],
+			[P2O.z, this.P2P0.z, this.P2P1.z]
+		];
+
+		let matA = [
+			[ray.d.x, P2O.x, this.P2P1.x],
+			[ray.d.y, P2O.y, this.P2P1.y],
+			[ray.d.z, P2O.z, this.P2P1.z]
+		];
+
+		let matB = [
+			[ray.d.x, this.P2P0.x, P2O.x],
+			[ray.d.y, this.P2P0.y, P2O.y],
+			[ray.d.z, this.P2P0.z, P2O.z]
+		];
+
+		let t = det(matT)/detEq;
+		let alpha = det(matA)/detEq;
+		let beta = det(matB)/detEq;
+
+		if (t < tmin || t > tmax) return null;
+		if (alpha < 0 || beta < 0 || (alpha+beta) > 1 || t < 0) return null
+
+		if (this.normSmooth) {
+			let n0cof = this.n0.clone();
+			n0cof.multiplyScalar(alpha);
+			let n1cof = this.n1.clone();
+			n1cof.multiplyScalar(beta);
+			let n2cof = this.n2.clone();
+			n2cof.multiplyScalar(1-alpha-beta);
+
+			this.normDir = n0cof;
+			this.normDir.add(n1cof);
+			this.normDir.add(n2cof);
+			this.normDir.normalize();
+		}
+
+		let isect = new Intersection();
+		isect.t = t;
+		isect.position = ray.pointAt(t);
+		isect.normal = this.normDir;
+		isect.material = this.material;
+		return isect;
+// ---YOUR CODE ENDS HERE---
+	}
+}
+
+function shapeLoadOBJ(objname, material, smoothnormal) {
+	loadOBJAsMesh(objname, function(mesh) { // callback function for non-blocking load
+		if(smoothnormal) mesh.computeVertexNormals();
+		for(let i=0;i<mesh.faces.length;i++) {
+			let p0 = mesh.vertices[mesh.faces[i].a];
+			let p1 = mesh.vertices[mesh.faces[i].b];
+			let p2 = mesh.vertices[mesh.faces[i].c];
+			if(smoothnormal) {
+				let n0 = mesh.faces[i].vertexNormals[0];
+				let n1 = mesh.faces[i].vertexNormals[1];
+				let n2 = mesh.faces[i].vertexNormals[2];
+				shapes.push(new Triangle(p0, p1, p2, material, n0, n1, n2));
+			} else {
+				shapes.push(new Triangle(p0, p1, p2, material));
+			}
+		}
+	}, function() {}, function() {});
+}
+
+/* ========================================
+ * You can define additional Shape classes,
+ * as long as each implements intersect function.
+ * ======================================== */
